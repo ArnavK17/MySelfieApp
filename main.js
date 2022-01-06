737161049ae3aed3eea2f1b86b4850a297f33d18
ArnavK17 <87708016+ArnavK17@users.noreply.github.com>
2022-01-06 18:36:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var SpeechRecognition = window.webkitSpeechRecognition; 
+var recognition = new SpeechRecognition();
+
+function start() { document.getElementById("textbox").innerHTML = "";
+ recognition.start();
+ } recognition.onresult = function(event) { console.log(event);
+     var Content = event.results[0][0].transcript;
+     document.getElementById("textbox").innerHTML = Content;
+     if (Content=="take my selfie"){
+        speak();
+        console.log("take my selfie")
+     }
+      console.log(Content);
+        }
+function speak(){
+    var synth=window.speechSynthesis;
+ speakdata="taking your selfie in 5 seconds"
+ var utterthis=new SpeechSynthesisUtterance(speakdata);
+synth.speak(utterthis);
+Webcam.attach(camera)
+setTimeout(function(){
+   takesnapshot();
+   save()
+},5000)
+}
+function save(){
+   link=document.getElementById("link")
+   image=document.getElementById("selfie_image").src
+   link.href=image
+link.click()
+}
+function takesnapshot(){
+   Webcam.snap(function (data_uri){
+   document.getElementById("result").innerHTML='<img id="selfie_image"src="'+data_uri+'">'})
+
+}
+Webcam.set({
+   width:360,
+   height:250,
+   image_format:'png',
+   png_quality:90
+})
+ camera=document.getElementById("camera")
+ 
